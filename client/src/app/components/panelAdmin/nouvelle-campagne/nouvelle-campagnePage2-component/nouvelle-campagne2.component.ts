0dc01d42320d95d8bdde836a0df7e0a1344abe82
MendozaMiguel <samirmiguelvoillaume@gmail.com>
2019-06-06 16:32:46 +0200

    change component for langue in form and post to API

--- client/src/app/components/panelAdmin/nouvelle-campagne/nouvelle-campagnePage2-component/nouvelle-campagne2.component.ts
+++ client/src/app/components/panelAdmin/nouvelle-campagne/nouvelle-campagnePage2-component/nouvelle-campagne2.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, EventEmitter, Output, Input } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormGroup, FormControl } from '@angular/forms';
 import { ApiClientService, API_URI_CAMPAIGNS } from '../../../../api-client/api-client.service';
+import { MatRadioChange, MatRadioButton } from '@angular/material';
 
 @Component({
   selector: 'app-NouvelleCampagnePage2Component',
@@ -9,48 +10,32 @@ import { ApiClientService, API_URI_CAMPAIGNS } from '../../../../api-client/api-
 })
 export class NouvelleCampagnePage2Component implements OnInit {
 
+  selectedLangue = "FR"
+
   @Output() incrementPage = new EventEmitter<any>();
   @Output() decrementPage = new EventEmitter<any>();
   @Input() formCampagne: FormGroup;
   public errorNomCampgane: boolean = false;
 
-  langsData: string[] = ["FR", "EN", "ES", "JP"];
-  lastAction: string;
-  constructor(public apiClientService: ApiClientService) {
-  }
+  constructor(public apiClientService: ApiClientService) { }
 
   ngOnInit() {
     this.formCampagne.patchValue({
       nomDeCampagne: this.formCampagne.value.role + " - " + this.formCampagne.value.experience
     })
   }
-  arrLang: any = [];
-
-  Change(event, item) {
-    // console.log(item + " _ " + event.checked);
-    if (event.checked) {
-      this.arrLang.push(item)
-    } else {
-      let index = this.arrLang.indexOf(item);
-      if (index > -1) {
-        this.arrLang.splice(index, 1);
-      }
-    }
-    // console.log(this.formCampagne.value.langs)
-    // console.log(this.arrLang);
-  }
 
   postData() {
-    // console.log(this.arrLang)
     this.formCampagne.patchValue({
       utilisationCopieColler: this.formCampagne.value.utilisationCopieColler,
       envoiRapportSimplifie: this.formCampagne.value.envoiRapportSimplifie,
-      langs: this.arrLang
+      langue: this.selectedLangue
     })
+    console.log(this.formCampagne.value.langues)
     this.apiClientService.post(API_URI_CAMPAIGNS, {
       "Name": this.formCampagne.value.nomDeCampagne,
       "level": this.formCampagne.value.experience,
-      "langs": this.formCampagne.value.langs,
+      "langs": this.formCampagne.value.langue,
       "copy_paste": this.formCampagne.value.utilisationCopieColler,
       "sent_report": this.formCampagne.value.envoiRapportSimplifie,
       "profile": this.formCampagne.value.role,
@@ -61,7 +46,9 @@ export class NouvelleCampagnePage2Component implements OnInit {
       },
       err => console.log(err)
     );
-    console.log("technologies " + this.formCampagne.value.techno)
+    console.log(this.formCampagne.value.langue)
+    console.log(this.formCampagne.value.role)
+    console.log(this.formCampagne.value)
     // console.log("cp " + this.formCampagne.value.utilisationCopieColler)
     // console.log("sent_rapport " + this.formCampagne.value.envoiRapportSimplifie)
     // console.log("langues " + this.formCampagne.value.langs)
