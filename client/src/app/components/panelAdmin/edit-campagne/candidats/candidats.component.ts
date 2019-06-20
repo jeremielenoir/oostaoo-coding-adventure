44e54b5db6b5e43970805dd53c9b82b63d03677c
MendozaMiguel <samirmiguelvoillaume@gmail.com>
2019-06-20 18:13:37 +0200

    update component add ngSwitch for View if have candidat in campaign or not

--- client/src/app/components/panelAdmin/edit-campagne/candidats/candidats.component.ts
+++ client/src/app/components/panelAdmin/edit-campagne/candidats/candidats.component.ts
@@ -2,6 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material';
 import { ActivatedRoute } from '@angular/router';
 import { InviteCandidat } from './invite-candidat.component';
+import {SelectionModel} from '@angular/cdk/collections';
+import {MatTableDataSource} from '@angular/material/table';
+import {
+  ApiClientService,
+  API_URI_CAMPAIGNS,
+} from '../../../../api-client/api-client.service';
 
 @Component({
   selector: 'app-candidats',
@@ -10,8 +16,20 @@ import { InviteCandidat } from './invite-candidat.component';
 })
 export class CandidatsComponent implements OnInit {
   public globalId: string;
+  public campaigns;
+  ViewCandidats = 'CandidatTrue';
+  choices = [
+    {value: 'exporter', viewValue: 'Exporter'},
+    {value: 'anonymiser', viewValue: 'Anonymiser'},
+    {value: 'supprimer', viewValue: 'Supprimer'}
+  ];
+  choicesTimeTest = [
+    {value: 'attente', viewValue: 'En attente'},
+    {value: 'terminer', viewValue: 'Terminés'},
+    {value: 'expirer', viewValue: 'Expirés'}
+  ];
 
-  constructor(public dialog: MatDialog, private route: ActivatedRoute) {
+  constructor(public dialog: MatDialog, private route: ActivatedRoute, public apiClientService: ApiClientService) {
     this.route.parent.params.subscribe(params => {
       this.globalId = params.id;
       // console.log('data', this.globalId);
@@ -25,5 +43,53 @@ export class CandidatsComponent implements OnInit {
     });
   }
 
-  ngOnInit() { }
+  ngOnInit() {
+    this.apiClientService
+    .get(API_URI_CAMPAIGNS + '/' + this.globalId)
+    .subscribe(datas => {
+      this.campaigns = [datas];
+      console.log('id campaign', this.campaigns);
+    });
+  }
+
+  displayedColumns: string[] = ['select', 'candidats', 'email', 'derniere_activite', 'score', 'technos'];
+  dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
+  selection = new SelectionModel<PeriodicElement>(true, []);
+
+  /** Whether the number of selected elements matches the total number of rows. */
+  isAllSelected() {
+    const numSelected = this.selection.selected.length;
+    const numRows = this.dataSource.data.length;
+    return numSelected === numRows;
+  }
+
+  /** Selects all rows if they are not all selected; otherwise clear selection. */
+  masterToggle() {
+    this.isAllSelected() ?
+        this.selection.clear() :
+        this.dataSource.data.forEach(row => this.selection.select(row));
+  }
+
+  /** The label for the checkbox on the passed row */
+  checkboxLabel(row?: PeriodicElement): string {
+    if (!row) {
+      return `${this.isAllSelected() ? 'select' : 'deselect'} all`;
+    }
+    return `${this.selection.isSelected(row) ? 'deselect' : 'select'} row ${row.position + 1}`;
+  }
+
 }
+
+export interface PeriodicElement {
+  position: number;
+  candidats: string;
+  email: string;
+  derniere_activite: string;
+  score: number;
+  technos: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position : 1, candidats: 'test', email: 'test@test', derniere_activite: 'Hydrogen', score: 1.0079, technos: 'H'},
+  {position: 2, candidats: 'test2', email: 'test2@test', derniere_activite: 'Helium', score: 4.0026, technos: 'He'},
+];
