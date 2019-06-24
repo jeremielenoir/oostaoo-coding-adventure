116166d876ccb3554740eb19b6e1082d377e4f0c
MendozaMiguel <samirmiguelvoillaume@gmail.com>
2019-06-24 17:43:10 +0200

    fix for tslint

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,23 +1,23 @@
-import { NgModule } from '@angular/core'
-import { Routes, RouterModule } from '@angular/router'
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
-import { AccueilComponent } from './components/home/accueil/accueil.component'
-import { NouvelleCampagneComponent } from './components/panelAdmin/nouvelle-campagne/nouvelle-campagne.component'
-import { RouteComponentComponent2 } from './components/panelAdmin/route-component2/route-component.component'
-import { ProfilUtilisateurComponent } from './components/panelAdmin/profil-utilisateur/profil-utilisateur.component'
-import { ProtectionDeDonneesComponent } from './components/panelAdmin/protection-de-donnees/protection-de-donnees.component'
-import { FacturationComponent } from './components/panelAdmin/facturation/facturation.component'
-import { UtilisateursComponent } from './components/panelAdmin/compte-utilisateurs/utilisateurs.component'
-import { ProfilEntrepriseComponent } from './components/panelAdmin/profil-entreprise/profil-entreprise.component'
-import { EditCampagneComponent } from './components/panelAdmin/edit-campagne/edit-campagne.component'
-import { CandidatsComponent } from './components/panelAdmin/edit-campagne/candidats/candidats.component'
-import { QuestionsComponent } from './components/panelAdmin/edit-campagne/questions/questions.component'
-import { SettingsComponent } from './components/panelAdmin/edit-campagne/settings/settings.component'
-import { GeneralComponent } from './components/panelAdmin/edit-campagne/settings/general/general.component'
-import { PersonnalisationComponent } from './components/panelAdmin/edit-campagne/settings/personnalisation/personnalisation.component'
-import { ClientTestComponent } from './components/panelAdmin/client-test/client-test.component'
+import { AccueilComponent } from './components/home/accueil/accueil.component';
+import { NouvelleCampagneComponent } from './components/panelAdmin/nouvelle-campagne/nouvelle-campagne.component';
+import { RouteComponentComponent2 } from './components/panelAdmin/route-component2/route-component.component';
+import { ProfilUtilisateurComponent } from './components/panelAdmin/profil-utilisateur/profil-utilisateur.component';
+import { ProtectionDeDonneesComponent } from './components/panelAdmin/protection-de-donnees/protection-de-donnees.component';
+import { FacturationComponent } from './components/panelAdmin/facturation/facturation.component';
+import { UtilisateursComponent } from './components/panelAdmin/compte-utilisateurs/utilisateurs.component';
+import { ProfilEntrepriseComponent } from './components/panelAdmin/profil-entreprise/profil-entreprise.component';
+import { EditCampagneComponent } from './components/panelAdmin/edit-campagne/edit-campagne.component';
+import { CandidatsComponent } from './components/panelAdmin/edit-campagne/candidats/candidats.component';
+import { QuestionsComponent } from './components/panelAdmin/edit-campagne/questions/questions.component';
+import { SettingsComponent } from './components/panelAdmin/edit-campagne/settings/settings.component';
+import { GeneralComponent } from './components/panelAdmin/edit-campagne/settings/general/general.component';
+import { PersonnalisationComponent } from './components/panelAdmin/edit-campagne/settings/personnalisation/personnalisation.component';
+import { ClientTestComponent } from './components/panelAdmin/client-test/client-test.component';
 
-import { CandidatsFormComponent } from './components/panelAdmin/edit-campagne/candidats-form/candidats-form.component'
+import { CandidatsFormComponent } from './components/panelAdmin/edit-campagne/candidats-form/candidats-form.component';
 
 const routes: Routes = [
   {
@@ -83,10 +83,10 @@ const routes: Routes = [
     path: 'dashboard/utilisateurs',
     component: UtilisateursComponent
   }
-]
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
