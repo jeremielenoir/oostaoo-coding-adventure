63aec475951e096d601713a069a82854e772a124
MendozaMiguel <samirmiguelvoillaume@gmail.com>
2019-06-27 17:42:19 +0200

    update component for display all questions and question selected by campaing

--- client/src/app/components/panelAdmin/edit-campagne/questions/questions.component.ts
+++ client/src/app/components/panelAdmin/edit-campagne/questions/questions.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import {
+  ApiClientService,
+  API_URI_CAMPAIGNS,
+  API_URI_QUESTIONS
+} from '../../../../api-client/api-client.service';
 
 @Component({
   selector: 'app-questions',
@@ -6,10 +12,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./questions.component.css']
 })
 export class QuestionsComponent implements OnInit {
+  public globalId;
+  public campaing;
+  public questions;
+  public allQuestions;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute,  public apiClientService: ApiClientService) {
+    this.route.parent.params.subscribe(params => {
+      this.globalId = params.id;
+      console.log('data', this.globalId);
+    });
+   }
 
   ngOnInit() {
+    this.apiClientService
+      .get(API_URI_CAMPAIGNS + '/' + this.globalId)
+      .subscribe(datas => {
+        this.campaing = datas;
+        this.questions = datas.questions;
+        console.log('campaign', this.campaing);
+        console.log('question of campaign: ', this.questions);
+        // for (const  question of this.campaing[0]) {
+        //   console.log(question);
+        // }
+        for (const idTechno of this.campaing.technologies) {
+        console.log('idTechno from campaign: ', idTechno.id);
+        }
+      });
+    this.apiClientService.get(API_URI_QUESTIONS).subscribe(datas => {
+        this.allQuestions = datas;
+        for (const data of datas) {
+          console.log('id techno from question: ', data.technologies);
+        }
+      });
   }
-
 }
