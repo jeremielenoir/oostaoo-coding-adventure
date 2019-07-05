c3fe1327dd98b3935ecad47c268dd2d1efbb6248
MendozaMiguel <samirmiguelvoillaume@gmail.com>
2019-07-05 17:21:49 +0200

    fix check buttons radio by data

--- client/src/app/components/panelAdmin/edit-campagne/settings/general/general.component.ts
+++ client/src/app/components/panelAdmin/edit-campagne/settings/general/general.component.ts
@@ -12,7 +12,7 @@ import { API_URI_CAMPAIGNS, ApiClientService } from 'src/app/api-client/api-clie
 })
 export class GeneralComponent implements OnInit {
 
-  constructor( private route: ActivatedRoute, public apiClientService: ApiClientService, public datepipe: DatePipe) {
+  constructor(private route: ActivatedRoute, public apiClientService: ApiClientService, public datepipe: DatePipe) {
     this.route.parent.parent.params.subscribe(params => {
       this.globalId = params.id;
     });
@@ -32,32 +32,59 @@ export class GeneralComponent implements OnInit {
   chrono = new FormControl('', Validators.required);
 
   ngOnInit() {
-    this.getCampaign().then( (campaigns) => {
-      this.name = new FormControl(campaigns[0].Name);
-      this.lang = new FormControl(campaigns[0].langs, );
-      this.copypaste = new FormControl(campaigns[0].copy_paste);
-      this.rapport = new FormControl(campaigns[0].sent_report);
-      console.log('form =', this.name.value, this.lang.value, this.copypaste.value, this.rapport.value );
-      console.log('campaign langue = ', this.lang.value);
-      this.dateExp = campaigns[0].expiration_date.slice(0, 10);
+    this.getCampaign().then(camp => {
+      this.name = new FormControl(camp[0].Name);
+      this.lang = new FormControl(camp[0].langs);
+      this.copypaste = new FormControl(camp[0].copy_paste);
+      this.rapport = new FormControl(camp[0].sent_report);
+      console.log('form =', this.name, this.lang.value, this.copypaste.value, this.rapport.value);
+      console.log('campaign selected in /general = ', camp);
+      console.log('campaign langue = ', camp[0].langs);
+      if (camp[0].langs === 'EN') {
+        this.EN = true;
+      }
+      if (camp[0].langs === 'FR') {
+        this.FR = true;
+      }
+      if (camp[0].langs === 'JP') {
+        this.JP = true;
+      }
+      if (camp[0].langs === 'ES') {
+        this.ES = true;
+      }
+
+      if (camp[0].copy_paste === true) {
+        this.copytrue = true;
+        this.copyfalse = false;
+      } else {
+        this.copytrue = false;
+        this.copyfalse = true;
+      }
+
+      if (camp[0].sent_report === true) {
+        this.rapporttrue = true;
+        this.rapportfalse = false;
+      } else {
+        this.rapporttrue = false;
+        this.rapportfalse = true;
+      }
+      this.dateExp = camp[0].expiration_date.slice(0, 10);
       this.NewDateExp = new Date(this.dateExp);
       // this.NewDateExp.setDate(this.NewDateExp.getDate() + this.datevalue);
       // console.log('new date exp: ', this.NewDateExp);
       console.log('datevalue =', this.datevalue);
-      console.log('langue', campaigns[0].langs);
     });
   }
 
-  // modifierPersonne() {
+  modifierPersonne() {
+     console.log('datevalue =', this.datevalue);	    
+    this.NewDateExp.setDate(this.NewDateExp.getDate() + this.datevalue);
+    console.log('date exp: ', this.campaigns[0].expiration_date.slice(0, 10));
+    console.log('new date exp: ', this.NewDateExp);
+    console.log('form =', this.name.value, this.lang, this.copypaste.value, this.rapport.value );
+	   }
 
-  //   console.log('datevalue =', this.datevalue);
-  //   this.NewDateExp.setDate(this.NewDateExp.getDate() + this.datevalue);
-  //   console.log('date exp: ', this.campaigns[0].expiration_date.slice(0, 10));
-  //   console.log('new date exp: ', this.NewDateExp);
-  //   console.log('form =', this.name.value, this.lang, this.copypaste.value, this.rapport.value );
-  // }
-
-   async getCampaign(): Promise<any> {
+  getCampaign(): Promise<any> {
     return this.apiClientService
       .get(API_URI_CAMPAIGNS + '/' + this.globalId)
       .toPromise()
