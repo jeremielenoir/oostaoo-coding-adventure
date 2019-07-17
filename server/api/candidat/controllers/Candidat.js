5ac2ecc52f6851554e4cfd82b7db553422432097
MendozaMiguel <samirmiguelvoillaume@gmail.com>
2019-07-17 15:54:02 +0200

    send email title and content email by front

--- server/api/candidat/controllers/Candidat.js
+++ server/api/candidat/controllers/Candidat.js
@@ -1,15 +1,15 @@
-"use strict";
+'use strict';
 
-const nodemailer = require("nodemailer");
+const nodemailer = require('nodemailer');
 
-const crypto = require("crypto");
+const crypto = require('crypto');
 
 // Create reusable transporter object using SMTP transport.
 const transporter = nodemailer.createTransport({
-  service: "Gmail",
+  service: 'Gmail',
   auth: {
-    user: "lenoir.jeremie@oostaoo.com",
-    pass: "marijuana"
+    user: 'lenoir.jeremie@oostaoo.com',
+    pass: 'marijuana'
   }
 });
 
@@ -60,49 +60,43 @@ module.exports = {
    * @return {Object}
    */
 
-  create: async ctx => {
+  create: async (ctx) => {
+    // console.log(strapi.services.campaign);
+    //faire un get campaigns avec ctx.request.body.token? qui est l'id de la campaign?
     var idCampaignNom = ctx.request.body.token + ctx.request.body.Nom;
-    console.log("idCampaignNom: ", idCampaignNom);
+    // console.log('ctx.request.body.token: ', ctx.request.body.token);
+    
+    // console.log("idCampaignNom: ", idCampaignNom);
     var cryptoData = crypto
-      .createHash("md5")
+      .createHash('md5')
       .update(idCampaignNom)
-      .digest("hex");
-    console.log("cryptoData: ", cryptoData);
-    console.log("ctx.request.body: ", ctx.request.body);
+      .digest('hex');
+    // console.log("cryptoData: ", cryptoData);
+    // console.log('ctx.request.body: ', ctx.request.body);
+    let email_message = ctx.request.body.email_content;
+    let email_title = ctx.request.body.email_title;
+    ctx.request.body = {
+      Nom : ctx.request.body.Nom,
+      email : ctx.request.body.email,
+      token : ctx.request.body.token
+    };
     const depositObj = {
       ...ctx.request.body,
       token: cryptoData
     };
 
     try {
+      // console.log('ctx.request.body dans TRY: ', ctx.request.body);
+      // console.log(email_title);
       let candidat = await strapi.services.candidat.add(depositObj);
       const options = {
         to: ctx.request.body.email,
-        from: "lenoir.jeremie@gmail.com",
-        replyTo: "no-reply@strapi.io",
-        subject: "Use strapi email provider successfully",
-        html: `
-            Bonjour mendoza,
-
-              Votre candidature a retenu notre attention.
-
-              Dans le cadre de notre processus de recrutement, nous avons le plaisir de vous inviter à passer une évaluation technique.
-              Vous pourrez choisir le moment le plus approprié pour vous pour passer ce test.
-
-              Quand vous serez prêt(e), cliquez sur le lien ci- dessous pour accéder à la page d’accueil de votre session: <a href='http://localhost:4200/evaluate'>http://localhost:4200/evaluate?id={{cryptoData}}</a>.
-
-                Bonne chance !
-
-                Cordialement,
-                -----
-                [[name]]
-                [[poste]]
-                [[Entreprise]]
-          `
+        from: 'lenoir.jeremie@gmail.com',
+        replyTo: 'no-reply@strapi.io',
+        subject: email_title,
+        html: email_message
       };
-
       await transporter.sendMail(options);
-
       return candidat;
     } catch (e) {
       return null;
