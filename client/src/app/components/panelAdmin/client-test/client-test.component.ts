c8bb663cc89766439544ee97b874589d4cc67c1d
MendozaMiguel <samirmiguelvoillaume@gmail.com>
2019-07-23 13:20:45 +0200

    check token to show page

--- client/src/app/components/panelAdmin/client-test/client-test.component.ts
+++ client/src/app/components/panelAdmin/client-test/client-test.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import {
+  ApiClientService,
+  API_URI_CAMPAIGNS,
+  API_URI_CANDIDATS
+} from '../../../api-client/api-client.service';
 
 @Component({
   selector: 'app-client-test',
@@ -6,10 +12,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./client-test.component.css']
 })
 export class ClientTestComponent implements OnInit {
+  idParam: string;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private route: ActivatedRoute, private apiClientService: ApiClientService, private router: Router) {
+    this.route.queryParams.subscribe(params => {
+      this.idParam = params.id;
+    });
   }
 
+  ngOnInit() {
+    console.log('this.idParam : ', this.idParam);
+    this.getCandidats();
+  }
+
+  getCandidats() {
+    this.apiClientService.get(API_URI_CANDIDATS).toPromise().then(res => {
+      for (const candidat of res) {
+        if (candidat.token === this.idParam) {
+          this.router.navigate(['/evaluate?id=' + this.idParam]);
+        }
+        if (candidat.token !== this.idParam) {
+          this.router.navigate(['/home']);
+        }
+      }
+    });
+  }
 }
