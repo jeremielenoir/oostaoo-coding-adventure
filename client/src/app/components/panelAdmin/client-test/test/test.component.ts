c21b2bf6f66cacb12225fd2b9789683f79d7454c
MendozaMiguel <samirmiguelvoillaume@gmail.com>
2019-07-29 15:43:24 +0200

    fix tslint and add champ in candidat (duree)

--- client/src/app/components/panelAdmin/client-test/test/test.component.ts
+++ client/src/app/components/panelAdmin/client-test/test/test.component.ts
@@ -1,8 +1,4 @@
-import { Component, OnInit, ViewChild, ElementRef, Input } from '@angular/core';
-import {
-  ApiClientService,
-  API_URI_QUESTIONS
-} from '../../../../api-client/api-client.service';
+import { Component, OnInit, Input } from '@angular/core';
 @Component({
   selector: 'app-test',
   templateUrl: './test.component.html',
@@ -21,31 +17,23 @@ export class TestComponent implements OnInit {
   public CalculTimeTotal = 0;
   @Input() questionCampaign: any;
   @Input() public technoCampaign: any;
-  public language:string;
+  public language: string;
 
 
-  constructor(private apiClientService: ApiClientService) { }
+  constructor() { }
 
   ngOnInit() {
-    console.log('technoCampaign from TEST: ', this.technoCampaign);
-    console.log('questionCampaign from TEST: ', this.questionCampaign);
     this.questions = this.questionCampaign;
     this.question = this.questionCampaign[this.index];
     this.timeDanger = this.questionCampaign[0].time - 5;
     this.type = this.questionCampaign[0];
     this.Countertime();
-
-    console.log('question id',this.technoCampaign)
-
-   for(let techno of this.technoCampaign){
-
-     if(this.question.technologies == techno.id){
-
-       this.language = techno.name
-
-     }
-
-   }
+    console.log('question id', this.technoCampaign);
+    for (const techno of this.technoCampaign) {
+      if (this.question.technologies === techno.id) {
+        this.language = techno.name;
+      }
+    }
   }
 
 
@@ -94,16 +82,16 @@ export class TestComponent implements OnInit {
     }
 
     this.question = this.questions[this.index];
-      
-    for(let techno of this.technoCampaign){
 
-     if(this.question.technologies == techno.id){
+    for (const techno of this.technoCampaign) {
 
-       this.language = techno.name
+      if (this.question.technologies === techno.id) {
 
-     }
+        this.language = techno.name;
 
-   }
+      }
+
+    }
 
     this.timeDanger = this.questions[this.index].time - 5;
 
