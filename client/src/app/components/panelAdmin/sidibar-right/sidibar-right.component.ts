984052b4e8ba0891017d4fbb62ccb5aa1c8ac744
MendozaMiguel <samirmiguelvoillaume@gmail.com>
2019-08-01 13:12:52 +0200

    recup campaigns from deashboard

--- client/src/app/components/panelAdmin/sidibar-right/sidibar-right.component.ts
+++ client/src/app/components/panelAdmin/sidibar-right/sidibar-right.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-sidibar-right',
@@ -6,12 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sidibar-right.component.css']
 })
 export class SidibarRightComponent implements OnInit {
-
+  @Input() campaignsFromParent;
   public Isactive = false;
+  public candidats: Array<any>;
 
   constructor() { }
 
   ngOnInit() {
+    // console.log('this.campaignsFromParent SIDIBAR: ', this.campaignsFromParent);
+    const myArrayCandidat = [];
+    for (const campaign of this.campaignsFromParent) {
+      // console.log('campaigns: ', campaign);
+      for (const candidat of campaign.candidats) {
+        console.log('candidat', candidat);
+        myArrayCandidat.push(candidat);
+        this.candidats = myArrayCandidat;
+      }
+    }
   }
 
   public param_cog() {
