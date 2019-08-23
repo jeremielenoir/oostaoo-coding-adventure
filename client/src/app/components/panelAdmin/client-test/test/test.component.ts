defb3c82a0032bb2899d10ea4359678b92739188
MendozaMiguel <samirmiguelvoillaume@gmail.com>
2019-08-23 14:59:54 +0200

    update app for get question, reponse and check right reponse for radio and textearea

--- client/src/app/components/panelAdmin/client-test/test/test.component.ts
+++ client/src/app/components/panelAdmin/client-test/test/test.component.ts
@@ -26,6 +26,8 @@ export class TestComponent implements OnInit {
   public responseUser: string;
   public counterCheck = 0; // counter good response
   public counterTotal = 0;
+  public arrayReponseUser: any = [];
+  public test: boolean;
 
 
   constructor(private apiClientService: ApiClientService) { }
@@ -36,11 +38,13 @@ export class TestComponent implements OnInit {
     this.timeDanger = this.questionCampaign[0].time - 5;
     this.type = this.questionCampaign[0];
     this.Countertime();
-    console.log('techno id', this.technoCampaign);
+    // console.log('techno id', this.technoCampaign);
     // FIRST QUESTIONS
-    this.responses = this.question.content.split(', ');
-    console.log('this.responses: ', this.responses);
-    console.log('this.question: ', this.question);
+    if (this.question.content !== null) {
+      this.responses = this.question.content.split(', ');
+    }
+    // console.log('this.responses: ', this.responses);
+    // console.log('this.question: ', this.question);
     for (const techno of this.technoCampaign) {
       if (this.question.technologies === techno.id) {
         this.language = techno.name;
@@ -48,6 +52,36 @@ export class TestComponent implements OnInit {
     }
   }
 
+  checkCheckBoxvalue(event, response) {
+    // console.log('this.checkReponse: ', this.checkReponse);
+    const arrayGoodRep = this.question.answer_value.split(', ');
+    if (event.source.checked) {
+      this.arrayReponseUser.push(response);
+      this.arrayReponseUser.sort();
+      arrayGoodRep.sort();
+      console.log('this.arrayReponseUser.sort(): ', this.arrayReponseUser);
+      console.log('arrayGoodRep.sort(): ', arrayGoodRep);
+      this.checkLengthAndItemArray(this.arrayReponseUser, arrayGoodRep);
+    } else {
+      const element = this.arrayReponseUser.find(item => item === response);
+      if (element) {
+        this.arrayReponseUser.splice(this.arrayReponseUser.indexOf(element), 1);
+        this.checkLengthAndItemArray(this.arrayReponseUser, arrayGoodRep);
+      }
+    }
+  }
+
+  checkLengthAndItemArray(arrayUser, arrayReponse) {
+    if (arrayUser.length === arrayReponse.length &&
+      arrayUser.every((element1, index) =>
+        element1 === arrayReponse[index])) {
+      console.log('ok');
+      console.log('this.arrayReponseUser: ', arrayUser);
+      this.test = true;
+    } else {
+      this.test = false;
+    }
+  }
 
   Countertime() {
     this.stopTimeInterval = setInterval(() => {
@@ -61,13 +95,12 @@ export class TestComponent implements OnInit {
   }
 
   public QuestNext() {
-    this.counterTotal++;
-    console.log('this.question: ', this.question);
-    console.log('this.responseUser: ', this.responseUser);
-    if (this.responseUser === this.question.answer_value) {
-      console.log('ok');
-      this.counterCheck++;
-    }
+    console.log('this.test: ', this.test);
+    // console.log('this.checkReponse: ', this.checkReponse);
+    this.counterTotal++; // counter total questions
+    // console.log('this.question: ', this.question);
+    // console.log('this.responseUser: ', this.responseUser);
+    // check reponseUser
     this.Alltime.push(this.timedefault);
     this.Activetime = false;
     if (this.index < this.questions.length - 1) {
@@ -78,28 +111,39 @@ export class TestComponent implements OnInit {
     } else {
       if (this.index === this.questions.length - 1) {
         this.dateFinishTest = new Date().toISOString();
-        console.log('testdate: ', this.dateFinishTest);
+        // console.log('testdate: ', this.dateFinishTest);
         clearInterval(this.stopTimeInterval);
         for (const nbrtime of this.Alltime) {
-          console.log('chaque temp passe sur chaque question', nbrtime);
+          // console.log('chaque temp passe sur chaque question', nbrtime);
           this.CalculTimeTotal += nbrtime;
         }
-        console.log('this.CalculTimeTotal: ', this.CalculTimeTotal);
+        // console.log('this.CalculTimeTotal: ', this.CalculTimeTotal);
         this.postTimeTest(this.CalculTimeTotal);
       }
     }
     this.question = this.questions[this.index];
     // NEXT QUESTIONS
-    this.responses = this.question.content.trim().split(', ');
-    console.log('this.responses in QUESTNEXT: ', this.responses);
+    if (this.question.content !== null) {
+      this.responses = this.question.content.split(', ');
+    } else {
+      this.responses = this.question;
+    }
+    // console.log('this.responses in QUESTNEXT: ', this.responses);
     for (const techno of this.technoCampaign) {
       if (this.question.technologies === techno.id) {
         this.language = techno.name;
       }
     }
     this.timeDanger = this.questions[this.index].time - 5;
-    console.log('type ', this.question.type);
-    console.log('COUNTER CHECK: ', this.counterCheck);
+    // console.log('type ', this.question.type);
+    // console.log('COUNTER CHECK: ', this.counterCheck);
+    if (this.responseUser === this.question.answer_value ||
+      this.question.answer_value.includes(this.responseUser)) {
+      console.log('ok');
+      this.counterCheck++;
+    }
+    this.responseUser = '';
+    this.arrayReponseUser = [];
     console.log('Ton score est de: ' + this.counterCheck + ' / ' + this.counterTotal);
   }
   public fmtMSS(s) {
