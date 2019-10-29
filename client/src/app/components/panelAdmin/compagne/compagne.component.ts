d34c19628dbfead6544e7f83b50af0199abfc972
famadykebe <famady932@gmail.com>
2019-10-29 17:51:08 +0100

    component compagne update scss

--- client/src/app/components/panelAdmin/compagne/compagne.component.ts
+++ client/src/app/components/panelAdmin/compagne/compagne.component.ts
@@ -1,43 +1,51 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
-import { ApiClientService, API_URI_CAMPAIGNS, API_URI_USER } from '../../../api-client/api-client.service';
-import { InviteCandidat } from '../edit-campagne/candidats/invite-candidat.component';
-import { MatDialog } from '@angular/material';
-import { DecryptTokenService } from 'src/app/components/home/register/register.service';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
-import { AuthenticationService } from './../../home/register/service/auth.service';
+import { Component, OnInit, Output, EventEmitter } from "@angular/core";
+import {
+  ApiClientService,
+  API_URI_CAMPAIGNS,
+  API_URI_USER
+} from "../../../api-client/api-client.service";
+import { InviteCandidat } from "../edit-campagne/candidats/invite-candidat.component";
+import { MatDialog } from "@angular/material";
+import { DecryptTokenService } from "src/app/components/home/register/register.service";
+import { Observable } from "rxjs";
+import { map } from "rxjs/operators";
+import { AuthenticationService } from "./../../home/register/service/auth.service";
 
 @Component({
-  selector: 'app-compagne',
-  templateUrl: './compagne.component.html',
-  styleUrls: ['./compagne.component.css']
+  selector: "app-compagne",
+  templateUrl: "./compagne.component.html",
+  styleUrls: ["./compagne.component.scss"]
 })
 export class CompagneComponent implements OnInit {
-
   public campaigns = [];
   public searchHeader: string;
   @Output() campaignsChild = new EventEmitter<any>();
-  public searchText = '';
+  public searchText = "";
 
-  constructor(public apiClientService: ApiClientService, public dialog: MatDialog,
-              public decryptTokenService: DecryptTokenService,
-              public authenticationService: AuthenticationService) {
+  constructor(
+    public apiClientService: ApiClientService,
+    public dialog: MatDialog,
+    public decryptTokenService: DecryptTokenService,
+    public authenticationService: AuthenticationService
+  ) {
     this.searchHeader = null;
   }
 
   ngOnInit() {
     console.log(this.decryptTokenService.userId);
-    this.authenticationService.getCampaignsUser(this.decryptTokenService.userId).then(resultat => {
-      console.log('CONNECTED GET CAMPAING: ', resultat);
-      this.campaigns = resultat;
-      this.giveCampaigns();
-    });
+    this.authenticationService
+      .getCampaignsUser(this.decryptTokenService.userId)
+      .then(resultat => {
+        console.log("CONNECTED GET CAMPAING: ", resultat);
+        this.campaigns = resultat;
+        this.giveCampaigns();
+      });
   }
 
   openDialog(idCampaign) {
     const inviteCandidatDialog = this.dialog.open(InviteCandidat, {
       data: idCampaign,
-      height: '80vh'
+      height: "80vh"
     });
     // inviteCandidatDialog.afterClosed().subscribe((data) => {
     //   this.getCampaigns().then(datas => {
@@ -47,10 +55,9 @@ export class CompagneComponent implements OnInit {
   }
 
   giveCampaigns() {
-    console.log('CAMPAINGS GIVE CAMPAIGNS: ', this.campaigns);
+    console.log("CAMPAINGS GIVE CAMPAIGNS: ", this.campaigns);
     if (this.campaigns) {
       this.campaignsChild.emit(this.campaigns);
     }
   }
-
 }
