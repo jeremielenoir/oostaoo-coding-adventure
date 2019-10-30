e641762b973794f0dd866252c0edd10147afac7b
MendozaMiguel <samirmiguelvoillaume@gmail.com>
2019-10-30 17:22:59 +0100

    rename file css to scss, import, rename id -> class  container

--- client/src/app/components/panelAdmin/client-test/fin-test/fin-test.component.ts
+++ client/src/app/components/panelAdmin/client-test/fin-test/fin-test.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-fin-test',
   templateUrl: './fin-test.component.html',
-  styleUrls: ['./fin-test.component.css']
+  styleUrls: ['./fin-test.component.scss']
 })
 export class FinTestComponent implements OnInit {
   public myStars = ['Très mauvaise', 'Mauvaise', 'Moyenne', 'Bonne', 'Très bonne'];
