10dfd6e50d1ac13503601d0ed1823d1e237a0319
Youcef GUEBOUL <ygueboul@gmail.com>
2020-01-15 15:52:11 +0100

    start stripe

--- client/src/app/components/home/offers/offers.component.ts
+++ client/src/app/components/home/offers/offers.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { ApiClientService } from 'src/app/api-client/api-client.service';
+import { ApiClientService, API_URI_OFFER } from 'src/app/api-client/api-client.service';
 
 @Component({
   selector: 'app-offers',
@@ -11,15 +11,30 @@ export class OffersComponent implements OnInit {
   handler: any = null;
   subscriptionChoice = null;
   subscriptionPage = false;
-  ListOffers = [];
+  listOffers = [];
 
   constructor(private router: Router, private apiClientService: ApiClientService) { }
 
+  public Stripe = window['Stripe'];
+
+  public stripe = this.Stripe('pk_test_jwK67X7FA3xfM8g4GxegZEVe00xbYkFsPq');
+
+  pay = (subscriptionAmount) => {
+    this.stripe.redirectToCheckout({
+      items: [{
+        // Define the product and SKU in the Dashboard first, and use the SKU
+        // ID in your client-side code.
+        plan: 'plan_123',
+        quantity: 1
+      }],
+      successUrl: 'https://www.example.com/success',
+      cancelUrl: 'https://www.example.com/cancel'
+    });
+  }
+
   ngOnInit() {
-    this.loadStripe();
-
+    this.apiClientService.get(API_URI_OFFER).subscribe( offers => {this.listOffers = offers; console.log(offers)});
     
-
     // recuperation de l'abonnement selectionné
     this.subscriptionChoice = localStorage.getItem('subscriptionChoice');
     // savoir si on est sur la page home ou sur la page abonnement interne (log)
@@ -35,7 +50,7 @@ export class OffersComponent implements OnInit {
     if (this.router.url.startsWith('/subscription')) {
       console.log('Nous sommes sur abonnement interne');
       this.pay(subscriptionAmount);
-      //this.router.navigate(['/dashboard/campaigns']);
+      // this.router.navigate(['/dashboard/campaigns']);
     } else {
       console.log('Nous sommes sur abonnement home');
       this.router.navigate(['/home/register']);
@@ -43,50 +58,6 @@ export class OffersComponent implements OnInit {
   }
 
 
-
-  // load <script src="https://checkout.stripe.com/checkout.js"></script> in appComponent
-  loadStripe() {
-    if(!window.document.getElementById('stripe-script')) {
-      var s = window.document.createElement('script');
-      s.id = 'stripe-script';
-      s.type = 'text/javascript';
-      s.src = 'https://checkout.stripe.com/checkout.js';
-      // s.onload = () => {
-      //   this.handler = (<any>window).StripeCheckout.configure({
-      //     key: 'pk_test_aeUUjYYcx4XNfKVW60pmHTtI',
-      //     locale: 'auto',
-      //     token: function (token: any) {
-      //       // You can access the token ID with `token.id`.
-      //       // Get the token ID to your server-side code for use.
-      //       console.log(token)
-      //       alert('Payment Success!!');
-      //     }
-      //   });
-      // }
-      window.document.body.appendChild(s);
-    }
-  }
-
-  pay(amount) {
-    var handler = (<any>window).StripeCheckout.configure({
-      key: 'pk_test_jwK67X7FA3xfM8g4GxegZEVe00xbYkFsPq',
-      locale: 'auto',
-      token: function (token: any) {
-        // You can access the token ID with `token.id`.
-        // Get the token ID to your server-side code for use.
-        console.log(token);
-        //this.router.navigate(['/dashboard/campaigns']);
-        alert('Token Created!!');
-      }
-    });
-
-    handler.open({
-      name: 'Roodeo',
-      description: 'payement',
-      amount: amount * 100,
-     // devise: 'EUR'
-    });
-  }
 
 
 
