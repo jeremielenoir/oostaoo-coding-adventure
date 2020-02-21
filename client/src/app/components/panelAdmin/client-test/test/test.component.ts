d640fa5b39eb4d245808b8d8831cd0c8afc3a890
Elodie <elodyjumel@gmail.com>
2020-02-21 14:44:05 +0100

    changement function convert time test component

--- client/src/app/components/panelAdmin/client-test/test/test.component.ts
+++ client/src/app/components/panelAdmin/client-test/test/test.component.ts
@@ -236,8 +236,13 @@ export class TestComponent implements OnInit {
     }
   }
 
-  public fmtMSS(s) {
-    return (s - (s %= 60)) / 60 + (9 < s ? ':' : ':0') + s;
+  public fmtMSS(d) {
+    d = Number(d);
+    var h = Math.floor(d/3600);
+    var m = Math.floor(d % 3600 / 60);
+    var s = Math.floor(d % 3600 % 60);
+
+    return ('0' + h).slice(-2) + ":" + ('0' + m).slice(-2) + ":" + ('0' + s).slice(-2);
   }
 
   postTimeTest(dureeTest) {
