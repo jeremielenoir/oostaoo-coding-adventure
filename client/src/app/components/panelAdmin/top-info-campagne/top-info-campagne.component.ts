29861a84a518a3a01f0fdf0ae1f4a4525dda7ce9
famadykebe <famady932@gmail.com>
2020-03-06 11:45:38 +0100

    create new component top-info

---
+++ client/src/app/components/panelAdmin/top-info-campagne/top-info-campagne.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-top-info-campagne',
+  templateUrl: './top-info-campagne.component.html',
+  styleUrls: ['./top-info-campagne.component.scss']
+})
+export class TopInfoCampagneComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
