33d52d0631cf2a4bac32ea39bda7dca8a029ad33
LuffyStar93 <luffy-star-93@hotmail.fr>
2020-03-16 14:46:49 +0100

    update question time to mm:ss

--- client/src/app/components/panelAdmin/question/question.component.ts
+++ client/src/app/components/panelAdmin/question/question.component.ts
@@ -108,13 +108,12 @@ export class QuestionComponent implements OnInit {
 
   fmtMSS(d) {
     d = Number(d);
-    var h = Math.floor(d / 3600);
+    // var h = Math.floor(d / 3600);
     var m = Math.floor((d % 3600) / 60);
     var s = Math.floor((d % 3600) % 60);
 
     return (
-      ("0" + h).slice(-2) +
-      ":" +
+
       ("0" + m).slice(-2) +
       ":" +
       ("0" + s).slice(-2)
