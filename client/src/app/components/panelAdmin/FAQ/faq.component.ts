3f02a6e999af9aa48528d90a6253076527aeb87d
MendozaMiguel <samirmiguelvoillaume@gmail.com>
2020-03-17 22:16:16 +0100

    FAQ -> update style

--- client/src/app/components/panelAdmin/FAQ/faq.component.ts
+++ client/src/app/components/panelAdmin/FAQ/faq.component.ts
@@ -1,16 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-
+import { API_URI_FAQ, ApiClientService } from 'src/app/api-client/api-client.service';
 @Component({
   selector: 'app-faq',
   templateUrl: './faq.component.html',
   styleUrls: ['./faq.component.scss']
 })
 export class FAQComponent implements OnInit {
+  panelOpenState = false;
+  faqs: any;
 
-
-  constructor() {
+  constructor(public apiClientService: ApiClientService) {
   }
 
   ngOnInit() {
+    this.apiClientService.get(API_URI_FAQ).toPromise().then(faqs =>{
+      console.log('faqs : ', faqs);
+      this.faqs = faqs;
+    });
   }
 }
