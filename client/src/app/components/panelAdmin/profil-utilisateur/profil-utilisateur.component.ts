a92a6a144564a5dfaa8b5be6da2a9bbce2dd821c
Elodie <elodyjumel@gmail.com>
2020-03-24 12:09:39 +0100

    page utilisateur - pseudo supprimé

--- client/src/app/components/panelAdmin/profil-utilisateur/profil-utilisateur.component.ts
+++ client/src/app/components/panelAdmin/profil-utilisateur/profil-utilisateur.component.ts
@@ -25,7 +25,6 @@ export class ProfilUtilisateurComponent implements OnInit {
   submittedEmail = false;
   prenom = new FormControl('', Validators.required);
   nom = new FormControl('', Validators.required);
-  username = new FormControl('', Validators.required);
   pays = new FormControl('', Validators.required);
   langue = new FormControl('', Validators.required);
   tel = new FormControl('', Validators.required);
@@ -43,7 +42,6 @@ export class ProfilUtilisateurComponent implements OnInit {
       console.log(user);
       this.prenom = new FormControl(user[0].prenom, Validators.required);
       this.nom = new FormControl(user[0].nom, Validators.required);
-      this.username = new FormControl(user[0].username, Validators.required);
       this.pays = new FormControl(user[0].pays, Validators.required);
       this.langue = new FormControl(user[0].langue);
       this.tel = new FormControl(user[0].tel);
@@ -64,13 +62,12 @@ export class ProfilUtilisateurComponent implements OnInit {
 
   updateprofil() {
     this.submittedProfil = true;
-    if (this.prenom.value === '' || this.nom.value === '' || this.username.value === '' || this.pays.value === '' || this.langue.value === 'value') {
+    if (this.prenom.value === '' || this.nom.value === '' || this.pays.value === '' || this.langue.value === 'value') {
       return console.log('Erreur veuillez remplir tout les champs requis');
     } else {
     this.apiClientService.put(API_URI_USER + '/' + this.decryptTokenService.userId, {
       Prenom: this.prenom.value,
       Nom: this.nom.value,
-      Username: this.username.value,
       Pays: this.pays.value,
       Langue: this.langue.value,
       Tel: this.tel.value,
