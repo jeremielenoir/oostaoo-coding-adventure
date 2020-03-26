b600ea968ade192cb83fc99a2b0a416397043929
MendozaMiguel <samirmiguelvoillaume@gmail.com>
2020-03-26 15:09:58 +0100

    Fix -> auto Open first accordion on filter

--- client/src/app/components/panelAdmin/FAQ/faq.pipes.ts
+++ client/src/app/components/panelAdmin/FAQ/faq.pipes.ts
@@ -4,11 +4,13 @@ import { Pipe, PipeTransform } from '@angular/core';
     name: 'typeFaq'
 })
 export class TypeFaqPipe implements PipeTransform {
+  newItems: Array<any>;
     transform(items: Array<any>, type: string): Array<any> {
     //   console.log('SHOW ITEM IN FILTERS : ', items);
       if(items && type){
-        items[0].open = true;
-        return items.filter(item => item.type === type);
+        this.newItems = items.filter(item => item.type === type);
+        this.newItems[0].open = true;
+        return this.newItems;
       }else{
         return items;
       }
