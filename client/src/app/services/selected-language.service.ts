f827215f04181f1b4b38043eaba6183320600c74
famadykebe <famady932@gmail.com>
2020-04-06 18:09:05 +0200

    cokies service add

--- client/src/app/services/selected-language.service.ts
+++ client/src/app/services/selected-language.service.ts
@@ -1,14 +1,31 @@
 import { Injectable } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SelectedLanguageService {
 
-  constructor() { }
+  constructor(private cookieService: CookieService) { }
 
-  recupLanguageCountry(){
-    let language = localStorage.getItem('lang');
-    return language;
+  getLanguageCountry(){
+
+    const cookies: any = this.cookieService.get('currentlanguage');
+
+    return cookies;
   }
+
+  checkLanguageCountry(){
+    return this.cookieService.check('currentlanguage')
+  }
+
+  updtateLanguageCountry(langage){
+    console.log('LANG', langage.codelang);
+   
+    this.cookieService.delete('currentlanguage');
+    this.cookieService.set('currentlanguage', langage.codelang);
+    // this.cookieService.set('currentlanguage', langage.codelang, 30, '/', 'roodeo.com', true, "Lax");
+  }
+
+
 }
