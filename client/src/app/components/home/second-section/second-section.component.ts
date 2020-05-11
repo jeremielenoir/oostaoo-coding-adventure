53b6011f272a8ae671e32e38a77ff9cd97b4483d
jeremielenoir <lenoir.jeremie@roodeo.com>
2020-05-11 21:24:10 +0200

    add animation

--- client/src/app/components/home/second-section/second-section.component.ts
+++ client/src/app/components/home/second-section/second-section.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2, ElementRef, ViewChild,} from '@angular/core';
+import { tween, styler, everyFrame, spring, stagger } from 'popmotion';
 
 @Component({
   selector: 'app-second-section',
@@ -6,10 +7,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./second-section.component.scss']
 })
 export class SecondSectionComponent implements OnInit {
+  
+  @ViewChild('second') second: ElementRef;
+  @ViewChild('third') third: ElementRef;
 
-  constructor() { }
+  constructor(private el: Renderer2, private elementRef: ElementRef) { }
 
   ngOnInit() {
+    
+    const ballStylers = Array
+    .from([this.second.nativeElement, this.third.nativeElement])
+    .map((element: string) => {
+      return styler(element);
+    });
+
+    /*const distance = 50;
+    const stagger = 0.5;
+    const speed = 0.01;
+
+    everyFrame()
+  .start((timestamp) => ballStylers.map((thisStyler: any, i) => {
+        thisStyler.set('y', distance * Math.sin(0.004 * timestamp + (i * 0.5)));
+    }));*/
+
+    const animations = Array(ballStylers.length)
+    .fill(spring({ to: '200px' }));
+
+    setInterval(() => {
+      stagger(animations, 2000)
+      .start((v) => v.forEach((x, i) => ballStylers[i].set('x', x)));
+    }, 5000);   
+      
+    
+    
+
   }
 
 }
