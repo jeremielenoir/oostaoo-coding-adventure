a9dab2fd20c888cdc027f90c8962386631b93484
dramanable <saliou58@live.fr>
2020-05-12 16:59:27 +0200

    second section animation

--- client/src/app/components/home/second-section/second-section.component.ts
+++ client/src/app/components/home/second-section/second-section.component.ts
@@ -1,46 +1,71 @@
-import { Component, OnInit, Renderer2, ElementRef, ViewChild,} from '@angular/core';
-import { tween, styler, everyFrame, spring, stagger } from 'popmotion';
+import {
+  Component,
+  OnInit,
+  Renderer2,
+  ElementRef,
+  ViewChild,
+} from "@angular/core";
+import { tween, styler, everyFrame, spring, stagger, easing } from "popmotion";
 
 @Component({
-  selector: 'app-second-section',
-  templateUrl: './second-section.component.html',
-  styleUrls: ['./second-section.component.scss']
+  selector: "app-second-section",
+  templateUrl: "./second-section.component.html",
+  styleUrls: ["./second-section.component.scss"],
 })
 export class SecondSectionComponent implements OnInit {
-  
-  @ViewChild('second') second: ElementRef;
-  @ViewChild('third') third: ElementRef;
+  @ViewChild("first") first: ElementRef;
+  @ViewChild("second") second: ElementRef;
+  @ViewChild("third") third: ElementRef;
 
-  constructor(private el: Renderer2, private elementRef: ElementRef) { }
+  constructor(private el: Renderer2, private elementRef: ElementRef) {}
 
   ngOnInit() {
-    
-    const ballStylers = Array
-    .from([this.second.nativeElement, this.third.nativeElement])
-    .map((element: string) => {
-      return styler(element);
-    });
+    // const ballStylers = Array
+    // .from([this.second.nativeElement, this.third.nativeElement])
+    // .map((element: string) => {
+    //   return styler(element);
+    // });
 
-    /*const distance = 50;
-    const stagger = 0.5;
-    const speed = 0.01;
+    // const animations = Array(ballStylers.length)
+    // .fill(spring({ to: '200px' }));
 
-    everyFrame()
-  .start((timestamp) => ballStylers.map((thisStyler: any, i) => {
-        thisStyler.set('y', distance * Math.sin(0.004 * timestamp + (i * 0.5)));
-    }));*/
-
-    const animations = Array(ballStylers.length)
-    .fill(spring({ to: '200px' }));
-
-    setInterval(() => {
-      stagger(animations, 2000)
-      .start((v) => v.forEach((x, i) => ballStylers[i].set('x', x)));
-    }, 5000);   
-      
-    
-    
-
+    // setInterval(() => {
+    //   stagger(animations, 2000)
+    //   .start((v) => v.forEach((x, i) => ballStylers[i].set('x', x)));
+    // }, 5000);
+    const duration = 1000;
+    const firstStyler = styler(this.first.nativeElement);
+    tween({
+       to: { x: 900 },
+      from: { x: 0 },
+      duration,
+      ease: easing.backOut,
+      flip: Infinity,
+      // elapsed: 500,
+      // loop: 5,
+      // yoyo: 5
+    }).start(firstStyler.set);
+    const secondStyler = styler(this.second.nativeElement);
+    tween({
+      to: { x: -200 },
+      from: { x: 200 },
+      duration,
+      ease: easing.backOut,
+      flip: Infinity,
+      // elapsed: 500,
+      // loop: 5,
+      // yoyo: 5
+    }).start(secondStyler.set);
+    const thirdStyler = styler(this.third.nativeElement);
+    tween({
+       to: { x: -200 },
+      from: { x: 200 },
+      duration,
+      ease: easing.backOut,
+      flip: Infinity,
+      // elapsed: 500,
+      // loop: 5,
+      // yoyo: 5
+    }).start(thirdStyler.set);
   }
-
 }
