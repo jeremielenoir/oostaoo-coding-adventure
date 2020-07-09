6e9d2312a8b60c281692c1344cf4829b9054cdbd
unknown <hamdoun.ismael@oostaoo.com>
2020-07-09 11:02:37 +0200

    interview added

---
+++ interview2/client/src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Interview from "./Interview";
+
+ReactDOM.render(<Interview />, document.getElementById("root"));
