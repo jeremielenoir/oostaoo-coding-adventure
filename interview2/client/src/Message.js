6d1a2ae8716ec1b7c167472e36590bf85718b3a3
Julian CARLE <carle.julian@gmail.com>
2020-07-20 16:00:34 +0200

    WebRTC video user/candidat DONE

--- interview2/client/src/Message.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './chatRoom.css';
-
-export default ({text, date}) => 
-<div className="message">
-   {text}
-    <span className="messageDate">{date}</span>
-</div>
