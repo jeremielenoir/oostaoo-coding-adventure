6aa70ff05092fec95a35ebfb81d77936b7531ceb
jeremielenoir <lenoir.jeremie@roodeo.com>
2020-07-22 17:15:25 +0200

    add slider button

--- client/src/app/material.ts
+++ client/src/app/material.ts
@@ -2,7 +2,7 @@ import {
     MatButtonModule, MatFormFieldModule, MatSelectModule, MatInputModule, MatRadioModule, MatCheckboxModule,
     MatIconModule, MatToolbarModule, MatMenuModule, MatListModule, MatSidenavModule, MatDialogModule,
     MatBottomSheetModule, MatTableModule, MatSortModule, MatTooltipModule, MAT_BOTTOM_SHEET_DEFAULT_OPTIONS,
-    MatDialogRef, MatCardModule, MatProgressSpinnerModule, MatSnackBarModule, MatExpansionModule,MatGridListModule,MatGridTile} from '@angular/material';
+    MatDialogRef, MatCardModule, MatProgressSpinnerModule, MatSnackBarModule, MatExpansionModule,MatGridListModule,MatGridTile, MatSlideToggleModule} from '@angular/material';
 
 import { DragDropModule } from '@angular/cdk/drag-drop';
 
@@ -12,11 +12,11 @@ import { NgModule } from '@angular/core';
     imports: [MatButtonModule, MatFormFieldModule, MatSelectModule, MatInputModule, MatRadioModule,
         MatCheckboxModule, MatIconModule, MatToolbarModule, MatMenuModule, MatListModule, MatSidenavModule,
         MatDialogModule, DragDropModule, MatBottomSheetModule, MatTableModule, MatSortModule, MatTooltipModule,
-        MatCardModule, MatProgressSpinnerModule, MatSnackBarModule, MatExpansionModule,MatGridListModule],
+        MatCardModule, MatProgressSpinnerModule, MatSnackBarModule, MatExpansionModule,MatGridListModule, MatSlideToggleModule],
     exports: [MatButtonModule, MatFormFieldModule, MatSelectModule, MatInputModule, MatRadioModule, MatCheckboxModule,
         MatIconModule, MatToolbarModule, MatMenuModule, MatListModule, MatSidenavModule, DragDropModule,
         MatBottomSheetModule, MatTableModule, MatSortModule, MatTooltipModule, MatDialogModule,
-        MatCardModule, MatProgressSpinnerModule, MatSnackBarModule, MatExpansionModule,MatGridListModule,MatGridTile],
+        MatCardModule, MatProgressSpinnerModule, MatSnackBarModule, MatExpansionModule,MatGridListModule,MatGridTile, MatSlideToggleModule],
     providers: [
         { provide: MAT_BOTTOM_SHEET_DEFAULT_OPTIONS, useValue: { hasBackdrop: false } },
         { provide: MatDialogRef, useValue: {} },
