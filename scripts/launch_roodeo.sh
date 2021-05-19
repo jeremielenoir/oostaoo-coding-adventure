3caba9b364e60e91cf10ef727c0479e0ce26ff37
Lenoir Jérémie <lenoir.jeremie@roodeo.com>
2021-05-19 16:22:00 +0200

    change build to push image to ecr

--- scripts/launch_roodeo.sh
+++ scripts/launch_roodeo.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 aws ecr get-login-password --region eu-west-3 | docker login --username AWS --password-stdin 164476836539.dkr.ecr.eu-west-3.amazonaws.com
 cd /home/ec2-user
+docker-compose pull
+docker-compose down
 docker-compose up -d
