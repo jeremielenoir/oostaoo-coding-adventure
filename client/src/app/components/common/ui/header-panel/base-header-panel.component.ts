470dc576cc42fe704b446a92a803227bceac4619
Lesieur Julien <lesieur.julien@orange.fr>
2021-10-20 15:28:24 +0200

    faq add contact #326

---
+++ client/src/app/components/common/ui/header-panel/base-header-panel.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'base-header-panel',
+  templateUrl: './base-header-panel.component.html',
+  styleUrls: ['./base-header-panel.component.scss']
+})
+export class BaseHeaderPanel implements OnInit {
+  constructor() { }
+
+  ngOnInit() {
+  }
+}
--- client/src/app/components/panelAdmin/FAQ/faq.component.ts
+++ client/src/app/components/panelAdmin/FAQ/faq.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { API_URI_FAQ, ApiClientService } from 'src/app/api-client/api-client.service';
 import { ActivatedRoute } from '@angular/router';
+
 @Component({
   selector: 'app-faq',
   templateUrl: './faq.component.html',
@@ -9,7 +10,7 @@ import { ActivatedRoute } from '@angular/router';
 export class FAQComponent implements OnInit {
   panelOpenState = false;
   faqs: any;
-  listTest : Array<String>;
+  listTest: Array<String>;
   filterParams: any;
   dataRoute: any;
   constructor(public apiClientService: ApiClientService, private route: ActivatedRoute) {
@@ -24,18 +25,20 @@ export class FAQComponent implements OnInit {
   ngOnInit() {
     // declaration nav route
     this.dataRoute = [
-      { routerLink : "/dashboard/faq", condition: true, classAnimParent: "hvr-icon-bounce", classAnimIcone: "hvr-icon", icon: "contact_support", name: "FAQ" },
-      { routerLink : "/dashboard/contact-support", condition: true, classAnimParent: "hvr-icon-bounce", classAnimIcone: "hvr-icon", icon: "place", name: "Contact" }
+      { routerLink: "/dashboard/faq", condition: true, classAnimParent: "hvr-icon-bounce", classAnimIcone: "hvr-icon", icon: "contact_support", name: "FAQ" },
+      { routerLink: "/dashboard/contact-support", condition: true, classAnimParent: "hvr-icon-bounce", classAnimIcone: "hvr-icon", icon: "place", name: "Contact" }
     ];
     this.apiClientService.get(API_URI_FAQ).toPromise().then(faqs => {
       // console.log('faqs : ', faqs);
       this.faqs = faqs;
       this.faqs[0].open = true;
       return this.listTest = Array.from(new Set(faqs.map(faq => faq.type)));
-    }).then(listType =>{
-      if(!listType.includes(this.filterParams)){
+    }).then(listType => {
+      if (!listType.includes(this.filterParams)) {
         this.filterParams = ''
       }
     });
+
+    console.log(this.filterParams);
   }
 }
