723e9045d23abb08f590504f0dd58ff983654c50
Lesieur Julien <lesieur.julien@orange.fr>
2021-11-10 09:35:42 +0100

    invitez candidats fixed

--- client/src/app/components/panelAdmin/edit-campagne/candidats/invite-candidat.component.ts
+++ client/src/app/components/panelAdmin/edit-campagne/candidats/invite-candidat.component.ts
@@ -1,8 +1,8 @@
-import { Component, EventEmitter, Inject, Input, Output } from '@angular/core';
-import { MatDialog, MatDialogRef } from '@angular/material';
+import { Component, OnInit, OnDestroy, Inject } from '@angular/core';
+import { MatDialogRef } from '@angular/material';
 import { Validators, FormControl } from '@angular/forms';
-import { ActivatedRoute } from '@angular/router';
 import { MAT_DIALOG_DATA } from '@angular/material';
+import { Subscription } from 'rxjs';
 import {
   ApiClientService,
   API_URI_CAMPAIGNS,
@@ -13,7 +13,8 @@ import {
   templateUrl: './invite-candidat.html',
   styleUrls: ['./invite-candidat.scss']
 })
-export class InviteCandidat {
+export class InviteCandidat implements OnInit, OnDestroy {
+  private subscription: Subscription;
   candidatID: any[];
   public campaigns: any;
   tests_available: number;
@@ -22,27 +23,22 @@ export class InviteCandidat {
     @Inject(MAT_DIALOG_DATA) public data: Record<string, any>,
     public dialogRef: MatDialogRef<InviteCandidat>,
     public apiClientService: ApiClientService
-  ) {
+  ) {}
+
+  emailFormControl = new FormControl('', [Validators.required, Validators.email]);
+  name_candidat = new FormControl('', [Validators.required]);
+
+  ngOnInit() {
+    this.subscription = this.apiClientService
+      .get(API_URI_CAMPAIGNS + '/' + this.data.globalId)
+      .subscribe(datas => this.campaigns = [datas]);
   }
 
-  emailFormControl = new FormControl('', [
-    Validators.required,
-    Validators.email
-  ]);
-  name_candidat = new FormControl('', [Validators.required]);
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
 
   close() {
     this.dialogRef.close(this.tests_available);
-  }
-
-  ngOnInit() {
-    // will log the entire data object
-    // console.log('params id', this.data); // show id pass in params router
-    this.apiClientService
-      .get(API_URI_CAMPAIGNS + '/' + this.data.globalId)
-      .subscribe(datas => {
-        this.campaigns = [datas];
-        // console.log('id campaign', this.campaigns);
-      });
   }
 }
