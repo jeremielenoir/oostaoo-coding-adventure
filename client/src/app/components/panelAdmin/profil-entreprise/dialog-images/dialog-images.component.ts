ee4d56bc176c18028d2ebdaa1ea881eb8f059127
MendozaMiguel <samirmiguelvoillaume@gmail.com>
2021-12-02 00:38:52 +0100

    show limit files, remove formData

--- client/src/app/components/panelAdmin/profil-entreprise/dialog-images/dialog-images.component.ts
+++ client/src/app/components/panelAdmin/profil-entreprise/dialog-images/dialog-images.component.ts
@@ -1,7 +1,7 @@
 import { ApiClientService, UPLOAD_TO_STRAPI } from 'src/app/api-client/api-client.service';
 import { Component, Inject, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import { MatDialogRef, MatSnackBar, MAT_DIALOG_DATA } from '@angular/material';
 import { DomSanitizer } from '@angular/platform-browser';
 import { DialogData } from '../../campagne/campagne.component';
 
@@ -19,28 +19,34 @@ export class DialogImagesComponent implements OnInit {
   limit_size = 0;
   type_size = '';
   dataFormFile = {};
+  submit: boolean;
 
   constructor(public dialogRef: MatDialogRef<DialogImagesComponent>, private formBuilder: FormBuilder, private sanitizer: DomSanitizer,
-    @Inject(MAT_DIALOG_DATA) public data: DialogData, public apiClientService: ApiClientService) {
+    @Inject(MAT_DIALOG_DATA) public data: DialogData, public apiClientService: ApiClientService, private _snackBar: MatSnackBar) {
     this.imageForm = this.formBuilder.group({
       image: ['', Validators.required],
     });
   }
 
   ngOnInit() {
-    this.disabled = true;
-    // console.log('data : ', this.data);
+    this.submit = false;
     this.limit = this.data['limit'];
     this.limit_size = this.data['limit_size'];
     this.type_size = this.data['type_size'];
     this.dataFormFile = { refId: this.data['refId'], ref: this.data['ref'], field: this.data['field'] };
+    this.files = this.data['images'] ? this.data['images'] : [];
+    if (this.files.length < 1) {
+      this.disabled = true;
+    } else {
+      this.disabled = false;
+    }
+    console.log('this.files : ', this.files);
   }
 
   uploadFile(event) {
-    if (event.length <= this.limit) {
+    if (this.files.length + event.length <= this.limit) {
       for (let index = 0; index < event.length; index++) {
         const element = event[index];
-        // console.log('element : ', element);
         if (this.formatBytes(element.size) && element.type === 'image/png' ||
           this.formatBytes(element.size) && element.type === 'image/gif' ||
           this.formatBytes(element.size) && element.type === 'image/jpeg') {
@@ -48,7 +54,9 @@ export class DialogImagesComponent implements OnInit {
             name: element.name, url: this.sanitizer.bypassSecurityTrustUrl(window.URL.createObjectURL(element)),
             properties: element
           });
+          console.log('this.files : ', this.files);
         } else {
+          this.openSnackBar('Fichier trop volumineux ou type du fichier non accepté', 'Fermer');
           console.log('return "show popup" type unknown, limit size depassed'); // show limits? and type of files
         }
       }
@@ -62,6 +70,7 @@ export class DialogImagesComponent implements OnInit {
         this.disabled = true;
       }
     } else {
+      this.openSnackBar('Limite des fichiers dépassés', 'Fermer');
       console.log('return "show popup" limit files depassed'); // show limit files?
     }
   }
@@ -100,15 +109,18 @@ export class DialogImagesComponent implements OnInit {
     }
   }
 
-
   onSubmit() {
-    const formData = new FormData();
-    for (const file of this.files) {
-      formData.append('files', file.properties);
-      formData.append('refId', this.dataFormFile['refId']); // id of content type
-      formData.append('ref', this.dataFormFile['ref']); // name of content type
-      formData.append('field', this.dataFormFile['field']); // name of key for the content type
+    this.submit = true;
+
+    if (this.submit) {
+      this.closeDialog({ properties: this.dataFormFile, files: this.files, submit: this.submit });
     }
-    this.closeDialog({ formData: formData, field: this.dataFormFile['field'], files: this.files });
+  }
+
+  openSnackBar(message: string, action) {
+    this._snackBar.open(message, action, {
+      duration: 3000,
+      panelClass: ['mat-snack-bar-container']
+    });
   }
 }
