b23ee148e1360c708447355308fa154f87b491c2
HugoPortier <hugo_portier@hotmail.com>
2022-01-05 16:21:24 +0100

    switch page on close call and video

---
+++ interview/client/src/pages/InterviewDeconnect.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function InterviewDeconnect() {
+    return (
+        <div>
+            <h1>page de deconnection</h1>
+        </div>
+    )
+}
+
+export default InterviewDeconnect
