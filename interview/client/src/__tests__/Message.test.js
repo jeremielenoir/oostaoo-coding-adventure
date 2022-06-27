8abe0862b1b0da0f73748b64682a2f53d7afaed5
Benoit Balcon <benoitbalcon@hotmail.fr>
2022-06-27 11:38:20 +0200

    first test implementation

---
+++ interview/client/src/__tests__/Message.test.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Message from '../components/Message'
+import {render} from '@testing-library/react'
+
+describe('Message', () => {
+    test('Should render without crash', async () => {
+        render(<Message />)
+    })
+})
+
+
