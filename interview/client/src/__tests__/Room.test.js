7b6960245cf475627f9471ad0ffc968dea38a929
Julien-Allard <julalldev@gmail.com>
2022-07-18 17:33:25 +0200

    Reworked Room.test.js a little bit, ESLint error spotted

--- interview/client/src/__tests__/Room.test.js
+++ interview/client/src/__tests__/Room.test.js
@@ -1,30 +1,34 @@
-import React from 'react'
-import Room from '../components/Room'
-import {render, screen} from '@testing-library/react'
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Room from '../components/Room';
 
 describe('Room', () => {
-    test('Should render without crash', async () => {
-        const props = {
-            match: {
-                params: {
-                    hash: ''
-                }
-            }
-        }
+  beforeEach(() => {
+    const props = {
+      match: {
+        params: {
+          hash: '',
+        },
+      },
+    };
 
-        const mockMedia = {
-            getUserMedia: jest.fn().mockImplementation(() =>
-                Promise.resolve(
-                    "stream"
-                ))
-        }
-        global.navigator.mediaDevices = mockMedia
+    const mockMedia = {
+      getUserMedia: jest
+        .fn()
+        .mockImplementation(() => Promise.resolve('stream')),
+    };
+    global.navigator.mediaDevices = mockMedia;
 
-        render(<Room {...props} hash="2"/>)
+    // eslint-disable-line
+    // disabling the "spreading props is forbidden" eslint error
+    render(<Room {...props} hash="2" />);
+  });
 
-        // find Roodeo logo
-        const roodeoLogo = await screen.findByRole("img", {name: /roodeo logo/i})
-        expect(roodeoLogo).toBeInTheDocument()
-
-    }) 
-})
+  // ESLint problem next line with "Parsing error: Unexpected token =>" for arrow function
+  // could maybe be corrected by changing parserOptions.ecmaVersion from 6 to 2020 (or 11) in .eslintrc.json
+  test('Should render without crash (with logo)', async () => {
+    // find Roodeo logo
+    const roodeoLogo = await screen.findByRole('img', { name: /roodeo logo/i });
+    expect(roodeoLogo).toBeInTheDocument();
+  });
+});
