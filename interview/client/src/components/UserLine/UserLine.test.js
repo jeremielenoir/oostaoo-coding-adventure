9ae8df4c04452125f42a80b45f67ff5fe6bb9107
Julien-Allard <julalldev@gmail.com>
2022-07-22 11:12:17 +0200

    Corrected my import mistake in UserLine.test.js and reworked tests

--- interview/client/src/components/UserLine/UserLine.test.js
+++ interview/client/src/components/UserLine/UserLine.test.js
@@ -1,51 +1,27 @@
 import React from 'react';
-import { render } from 'react-dom';
+import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
-
-import ListItem from '@material-ui/core/ListItem';
-import ListItemAvatar from '@material-ui/core/ListItemAvatar';
-import Avatar from '@material-ui/core/Avatar';
-import ListItemText from '@material-ui/core/ListItemText';
 
 import UserLine from './UserLine';
 
-// next line is because UserLine basically is just a bunch of MUI components and so is not considered as a DOM element
-// without this setup the test will console the "Target container is not a DOM element" error
-jest.mock('react-dom', () => {
-  return { render: jest.fn() };
-});
-
 describe('UserLine component tests', () => {
   test('should render correctly', () => {
-    render(<UserLine />);
+    render(<UserLine name="Toto" />);
   });
 
-  test('ListItem MUI component should render', () => {
-    render(<ListItem />);
+  test('should have an avatar representing the first letter of the name passed in props', () => {
+    render(<UserLine name="Toto" />);
+
+    const avatar = screen.getByText('T');
+
+    expect(avatar).toBeInTheDocument();
   });
 
-  test('ListItemAvatar MUI component should render', () => {
-    render(<ListItemAvatar />);
-  });
+  test('should display the name passed in props', () => {
+    render(<UserLine name="Toto" />);
 
-  test('Avatar MUI component should render', () => {
-    render(<Avatar />);
-  });
+    const userName = screen.getByText('Toto');
 
-  // TypeError: Cannot read properties of undefined (reading 'getByText')
-  // test("should display an avatar with the first letter of the user's name which is passed in props", () => {
-  //   const fakeName = 'Toto';
-  //   render(
-  //     <UserLine name={fakeName}>
-  //       <Avatar />
-  //     </UserLine>
-  //   );
-
-  //   const avatar = screen.getByText('T');
-  //   expect(avatar).toBeInTheDocument();
-  // });
-
-  test('ListItemText MUI component should render', () => {
-    render(<ListItemText />);
+    expect(userName).toBeInTheDocument();
   });
 });
