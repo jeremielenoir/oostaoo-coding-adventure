3f618d3c5acb1389f21ecc76eebdd447b52db3bd
Frier Benjamin <frier.benjamin@gmail.com>
2022-08-29 17:22:11 +0200

    Refacto socketRef and loggedOffPage (#487)
    Co-authored-by: JulAllDev <91843128+Julien-Allard@users.noreply.github.com>

--- interview/client/src/Pages/LoggedOffPage/LoggedOffPage.jsx
+++ interview/client/src/Pages/LoggedOffPage/LoggedOffPage.jsx
@@ -1,21 +1,16 @@
-import React, { useContext } from 'react';
+import React  from 'react';
 import { Link } from 'react-router-dom';
 /* MUI components */
 import Button from '@material-ui/core/Button';
-
-import { EndPointContext } from '../../useContext';
 
 /* Style */
 import './loggedOffPage.css';
 
 function LoggedOffPage() {
-  const msg = useContext(EndPointContext);
   return (
     <div className="container-disconnect">
       <h3>Vous avez quitté la réunion.</h3>
-      <h1>{msg}</h1>
       <div className="buttons">
-        {/* <Link to="/rooms/:hash"> */}
         <Link to="/rooms/2">
           <Button variant="outlined">Retour vers l'accueil</Button>
         </Link>
