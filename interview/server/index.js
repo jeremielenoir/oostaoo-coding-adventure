8c24c925a6452d1ca8482bc6ab32960c97a86883
Julien-Allard <julalldev@gmail.com>
2022-09-07 10:09:59 +0200

    FIX/Forgot to push server part

--- interview/server/index.js
+++ interview/server/index.js
@@ -17,7 +17,7 @@ const io = require('socket.io').listen(httpServer);
 const moment = require('moment');
 const config = require('./config');
 
-const rooms = {};
+const rooms = [];
 
 /* pour plus tard
 var room = io.sockets.in('some super awesome room');
@@ -43,6 +43,21 @@ io.on('connection', (socket) => {
   io.emit('FromApi', messages);
   socket.emit('my-id', socket.id);
 
+  socket.on('disconnect', () => {
+    socket.broadcast.emit('CallEnded');
+  });
+
+  socket.on('send-call', (data) => {
+    io.to(data.userToCall).emit('send-call', {
+      signal: data.signalData,
+      from: data.from,
+    });
+  });
+
+  socket.on('receive-call', (data) => {
+    io.to(data.to).emit('call-received', data.signal);
+  });
+
   socket.on('newMessage', (message) => {
     if (messages.length === 30) {
       messages.shift();
@@ -59,32 +74,56 @@ io.on('connection', (socket) => {
 
   socket.on('join room', (roomID) => {
     console.log(`User ${socket.id} joined room ${roomID}`);
-    if (rooms[roomID]) {
-      rooms[roomID].push(socket.id);
+
+    const newRoom = {
+      id: roomID,
+      users: [socket.id],
+    };
+
+    const doesRoomExists = rooms.find((room) => room.id === roomID);
+    if (doesRoomExists) {
+      const amIConnected = doesRoomExists.users.find(
+        (user) => user === socket.id,
+      );
+      if (!amIConnected) {
+        doesRoomExists.users.push(socket.id);
+      }
+
+      const otherUser = doesRoomExists.users.find((user) => user !== socket.id);
+      if (otherUser) {
+        socket.emit('other user', otherUser);
+        console.log('Room partner : ', otherUser);
+      }
     } else {
-      rooms[roomID] = [socket.id];
+      rooms.push(newRoom);
     }
+
+    // if (rooms[roomID]) {
+    //   rooms[roomID].push(socket.id);
+    // } else {
+    //   rooms[roomID] = [socket.id];
+    // }
 
     io.emit('FromAPI', messages);
 
-    const otherUser = rooms[roomID].find((id) => id !== socket.id);
-    if (otherUser) {
-      socket.emit('other user', otherUser);
-      socket.to(otherUser).emit('user joined', socket.id);
-    }
+    // const otherUser = rooms[roomID].find((id) => id !== socket.id);
+    // if (otherUser) {
+    //   socket.emit('other user', otherUser);
+    //   socket.to(otherUser).emit('user joined', socket.id);
+    // }
   });
 
-  socket.on('offer', (payload) => {
-    io.to(payload.target).emit('offer', payload);
-  });
+  // socket.on('offer', (payload) => {
+  //   io.to(payload.target).emit('offer', payload);
+  // });
 
-  socket.on('answer', (payload) => {
-    io.to(payload.target).emit('answer', payload);
-  });
+  // socket.on('answer', (payload) => {
+  //   io.to(payload.target).emit('answer', payload);
+  // });
 
-  socket.on('ice-candidate', (incoming) => {
-    io.to(incoming.target).emit('ice-candidate', incoming.candidate);
-  });
+  // socket.on('ice-candidate', (incoming) => {
+  //   io.to(incoming.target).emit('ice-candidate', incoming.candidate);
+  // });
 });
 
 httpServer.listen(config.PORT, config.HOST, () => {
