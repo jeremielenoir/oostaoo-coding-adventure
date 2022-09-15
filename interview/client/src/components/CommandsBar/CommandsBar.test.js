a71a0fec7df225f669f09e45ed5653e25bf7d0d7
JulAllDev <91843128+Julien-Allard@users.noreply.github.com>
2022-09-15 14:08:29 +0200

    FEAT/tests ok (#503)
    * FEAT/tests ok
    
    * Update CommandsBar.test.js
    
    Co-authored-by: Frier Benjamin <frier.benjamin@gmail.com>

--- interview/client/src/components/CommandsBar/CommandsBar.test.js
+++ interview/client/src/components/CommandsBar/CommandsBar.test.js
@@ -1,20 +1,35 @@
 import React from 'react';
 import '@testing-library/jest-dom';
 import { fireEvent, render, screen } from '@testing-library/react';
-import { BrowserRouter } from 'react-router-dom';
 import CommandsBar from './CommandsBar';
-import { StreamContextProvider } from '../../common/StreamContext';
+
+// Important to test everything coming from StreamContext
+import { StreamContext } from '../../common/StreamContext';
+import {
+  micOn,
+  micToggle,
+  videoCamOn,
+  videoCamToggle,
+} from '../../common/StreamContext';
 
 describe('CommandsBar component tests', () => {
   test('should render correctly', () => {
-    render(<CommandsBar />);
+    render(
+      <StreamContext.Provider
+        value={{ micOn, micToggle, videoCamOn, videoCamToggle }}
+      >
+        <CommandsBar />
+      </StreamContext.Provider>
+    );
   });
 
   test('should have a button to activate mic', () => {
     render(
-      <StreamContextProvider>
+      <StreamContext.Provider
+        value={{ micOn, micToggle, videoCamOn, videoCamToggle }}
+      >
         <CommandsBar />
-      </StreamContextProvider>
+      </StreamContext.Provider>
     );
 
     const micOnButton = screen.getByTestId('micOnBtn');
@@ -25,8 +40,13 @@ describe('CommandsBar component tests', () => {
   });
 
   test('should have a button to deactivate mic instead of the mic on one if the micOn prop is true', () => {
-    render(<CommandsBar micOn />);
-
+    render(
+      <StreamContext.Provider
+        value={{ micOn: true, micToggle, videoCamOn, videoCamToggle }}
+      >
+        <CommandsBar />
+      </StreamContext.Provider>
+    );
     const micOffButton = screen.getByTestId('micOffBtn');
     const micOnButton = screen.queryByTestId('micOnBtn');
 
@@ -34,9 +54,15 @@ describe('CommandsBar component tests', () => {
     expect(micOnButton).not.toBeInTheDocument();
   });
 
-  test('The mic on button should trigger the micToggle function passed in props', () => {
+  test('The mic on button should trigger the micToggle function passed via context', () => {
     const mockFunction = jest.fn();
-    render(<CommandsBar micToggle={mockFunction} />);
+    render(
+      <StreamContext.Provider
+        value={{ micOn, micToggle: mockFunction, videoCamOn, videoCamToggle }}
+      >
+        <CommandsBar />
+      </StreamContext.Provider>
+    );
 
     const micOnButton = screen.getByTestId('micOnBtn');
     fireEvent.click(micOnButton);
@@ -44,9 +70,20 @@ describe('CommandsBar component tests', () => {
     expect(mockFunction).toHaveBeenCalledTimes(1);
   });
 
-  test('The mic off button should trigger the micToggle function passed in props', () => {
+  test('The mic off button should also trigger the micToggle function passed via context', () => {
     const mockFunction = jest.fn();
-    render(<CommandsBar micOn micToggle={mockFunction} />);
+    render(
+      <StreamContext.Provider
+        value={{
+          micOn: true,
+          micToggle: mockFunction,
+          videoCamOn,
+          videoCamToggle,
+        }}
+      >
+        <CommandsBar />
+      </StreamContext.Provider>
+    );
 
     const micOffButton = screen.getByTestId('micOffBtn');
     fireEvent.click(micOffButton);
@@ -54,8 +91,14 @@ describe('CommandsBar component tests', () => {
     expect(mockFunction).toHaveBeenCalledTimes(1);
   });
 
-  test('should have a button to desactive videoCam', () => {
-    render(<CommandsBar />);
+  test('should have a videoCamOn button', () => {
+    render(
+      <StreamContext.Provider
+        value={{ micOn, micToggle, videoCamOn, videoCamToggle }}
+      >
+        <CommandsBar />
+      </StreamContext.Provider>
+    );
 
     const videoCamOnButton = screen.getByTestId('videoCamOnBtn');
     const videoCamOffButton = screen.queryByTestId('videoCamOffBtn');
@@ -64,8 +107,14 @@ describe('CommandsBar component tests', () => {
     expect(videoCamOffButton).not.toBeInTheDocument();
   });
 
-  test('should have a button to deactivate videoCam instead of the videoCam on one if the videoCamOn prop is true', () => {
-    render(<CommandsBar videoCamOn />);
+  test('should have a videoCamOff button instead of the videoCamOn one if the videoCamOn prop is true', () => {
+    render(
+      <StreamContext.Provider
+        value={{ micOn, micToggle, videoCamOn: true, videoCamToggle }}
+      >
+        <CommandsBar />
+      </StreamContext.Provider>
+    );
 
     const videoCamOffButton = screen.getByTestId('videoCamOffBtn');
     const videoCamOnButton = screen.queryByTestId('videoCamOnBtn');
@@ -74,9 +123,15 @@ describe('CommandsBar component tests', () => {
     expect(videoCamOnButton).not.toBeInTheDocument();
   });
 
-  test('The videoCam on button should trigger the videoCamToggle function passed in props', () => {
+  test('The videoCam on button should trigger the videoCamToggle function passed via context', () => {
     const mockFunction = jest.fn();
-    render(<CommandsBar videoCamToggle={mockFunction} />);
+    render(
+      <StreamContext.Provider
+        value={{ micOn, micToggle, videoCamOn, videoCamToggle: mockFunction }}
+      >
+        <CommandsBar />
+      </StreamContext.Provider>
+    );
 
     const videoCamOnButton = screen.getByTestId('videoCamOnBtn');
     fireEvent.click(videoCamOnButton);
@@ -84,9 +139,20 @@ describe('CommandsBar component tests', () => {
     expect(mockFunction).toHaveBeenCalledTimes(1);
   });
 
-  test('The videoCam on button should trigger the videoCamToggle function passed in props', () => {
+  test('The videoCam on button should also trigger the videoCamToggle function passed via context', () => {
     const mockFunction = jest.fn();
-    render(<CommandsBar videoCamOn videoCamToggle={mockFunction} />);
+    render(
+      <StreamContext.Provider
+        value={{
+          micOn,
+          micToggle,
+          videoCamOn: true,
+          videoCamToggle: mockFunction,
+        }}
+      >
+        <CommandsBar />
+      </StreamContext.Provider>
+    );
 
     const videoCamOffButton = screen.getByTestId('videoCamOffBtn');
     fireEvent.click(videoCamOffButton);
@@ -96,9 +162,11 @@ describe('CommandsBar component tests', () => {
 
   test('should have a button to end call', () => {
     render(
-      <BrowserRouter>
+      <StreamContext.Provider
+        value={{ micOn, micToggle, videoCamOn, videoCamToggle }}
+      >
         <CommandsBar />
-      </BrowserRouter>
+      </StreamContext.Provider>
     );
 
     const button = screen.getByTestId('callEndButton');
@@ -107,28 +175,44 @@ describe('CommandsBar component tests', () => {
   });
 
   test('should have a toggle users button in the footer', () => {
-    const { container } = render(<CommandsBar />);
+    const { container } = render(
+      <StreamContext.Provider
+        value={{ micOn, micToggle, videoCamOn, videoCamToggle }}
+      >
+        <CommandsBar />
+      </StreamContext.Provider>
+    );
 
     const toggleUsersBtn = container.getElementsByClassName('footer-icons');
 
     expect(toggleUsersBtn).toBeTruthy();
   });
 
-  test('should have a toggle users and a toggle chat button in the footer', () => {
-    render(<CommandsBar />);
+  test('should have a toggle chat button in the footer', () => {
+    render(
+      <StreamContext.Provider
+        value={{ micOn, micToggle, videoCamOn, videoCamToggle }}
+      >
+        <CommandsBar />
+      </StreamContext.Provider>
+    );
 
-    const toggleUsersBtn = screen.getByTestId('group-icon');
     const toggleChatBtn = screen.getByTestId('chat-icon');
 
-    expect(toggleUsersBtn).toBeInTheDocument();
     expect(toggleChatBtn).toBeInTheDocument();
   });
 
   test('click on toggle users button should trigger the toggleParticipant function passed in props', () => {
     const mockFunction = jest.fn();
-    render(<CommandsBar toggleParticipant={mockFunction} />);
+    const { container } = render(
+      <StreamContext.Provider
+        value={{ micOn, micToggle, videoCamOn, videoCamToggle }}
+      >
+        <CommandsBar toggleParticipant={mockFunction} />
+      </StreamContext.Provider>
+    );
 
-    const toggleUsersBtn = screen.getByTestId('group-icon');
+    const toggleUsersBtn = container.getElementsByClassName('footer-icons')[0];
     fireEvent.click(toggleUsersBtn);
 
     expect(mockFunction).toHaveBeenCalledTimes(1);
@@ -136,7 +220,13 @@ describe('CommandsBar component tests', () => {
 
   test('click on toggle chat button should trigger the toggleMessage function passed in props', () => {
     const mockFunction = jest.fn();
-    render(<CommandsBar toggleMessage={mockFunction} />);
+    render(
+      <StreamContext.Provider
+        value={{ micOn, micToggle, videoCamOn, videoCamToggle }}
+      >
+        <CommandsBar toggleMessage={mockFunction} />
+      </StreamContext.Provider>
+    );
 
     const toggleChatBtn = screen.getByTestId('chat-icon');
     fireEvent.click(toggleChatBtn);
