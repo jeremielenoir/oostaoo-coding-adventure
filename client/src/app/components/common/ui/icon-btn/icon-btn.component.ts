7b9101df915416e5b0523fbffd9dbf7b9feebe09
LenoirJehan75 <>
2023-02-14 16:23:14 +0100

    version14/02/2023

---
+++ client/src/app/components/common/ui/icon-btn/icon-btn.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+    selector: 'icon-btn',
+    templateUrl: './icon-btn.component.html',
+    styleUrls: ['./icon-btn.component.scss']
+})
+
+export class iconbtn {
+
+}
